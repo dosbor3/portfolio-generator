1ae8efd9c24d781cf1ecbb0b1ac2ac7ea098907a
I am Billie <dosbor3@gmail.com>
2022-01-23 15:03:16 -0600

    validation and when methods added

--- app.js
+++ app.js
@@ -21,16 +21,58 @@ const promptUser = () => {
       name: 'github',
       message: 'Enter your GitHub username?'
     },
+    /*
+    One last step remains. Let's ask the user if they'd like to add an About section. Then we'll prompt the 
+    user for information, but only after they confirm yes. First, we must add a confirmation type question 
+    and then ask for the information.
+
+    Let's add the following confirmation question to the promptUser() function just below the GitHub username 
+    prompt in the app.js file:  */
     {
-      type: 'input',
-      name: 'about',
-      message: 'Provide some information about yourself?'
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to enter some information about yourself for an "About" section?',
+      default: true
+    },
+
+    /*
+    Next we'll add another property to the prompt looking for information about the user called when. 
+    This is like the validate method we used previously, but instead of passing the value entered for 
+    that specific question in as the parameter, it passes an object of all of the answers given so far as an object.
+
+    Edit the existing about input to incorporate the when property, as shown here:
+
+    {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to enter some information about yourself for an "About" section?',
+      default: true
     },
     {
       type: 'input',
-      name: 'name',
-      message: 'What is your name?'
+      name: 'about',
+      message: 'Provide some information about yourself:',
+      when: ({ confirmAbout }) => {
+        if (confirmAbout) {
+          return true;
+        } else {
+          return false;
+        }
+      }
     }
+    */
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Would you like to provide some information about yourself?',
+      when: ({ confirmAbout }) => {
+        if (confirmAbout) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    }    
   ]);
 };
 
@@ -56,7 +98,7 @@ Add a New Project
           return true;
         }
         else {
-          console.log("Please enter your name!");
+          console.log("Please enter your project name!");
           return false;
         }
       }
@@ -71,7 +113,7 @@ Add a New Project
           return true;
         }
         else {
-          console.log("Please enter your name!");
+          console.log("Please enter a description of your project!");
           return false;
         }
       }
@@ -91,7 +133,7 @@ Add a New Project
           return true;
         }
         else {
-          console.log("Please enter your name!");
+          console.log("Please enter your github  name!");
           return false;
         }
       }
