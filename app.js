91ac46626513f534512cd1c964b7a8697e4ab200
I am Billie <dosbor3@gmail.com>
2022-01-23 13:50:07 -0600

    validation added

--- app.js
+++ app.js
@@ -5,7 +5,16 @@ const promptUser = () => {
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name?'
+      message: 'What is your name?',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        }
+        else {
+          console.log("Please enter your name!");
+          return false;
+        }
+      }
     },
     {
       type: 'input',
@@ -51,11 +60,21 @@ Add a New Project
           return false;
         }
       }
+      
     },
     {
       type: 'input',
       name: 'description',
-      message: 'Provide a description of the project (Required)'
+      message: 'Provide a description of the project (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        }
+        else {
+          console.log("Please enter your name!");
+          return false;
+        }
+      }
     },
     {
       type:"checkbox",
@@ -66,7 +85,16 @@ Add a New Project
     {
       type:"input",
       name: "link", 
-      message: "Enter the GitHub link to your project.  (Required)"
+      message: "Enter the GitHub link to your project.  (Required)",
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        }
+        else {
+          console.log("Please enter your name!");
+          return false;
+        }
+      }
     },
     {
       type: 'confirm',
